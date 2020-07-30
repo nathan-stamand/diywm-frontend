f7ba224904299f531f5c8ae6a3bb1062a67f6be3
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 08:23:21 -0500

    filled in const loadProjects to fetch projects from db then call dispatch on action 'ADD_PROJECTS'

--- src/actions/projects.js
+++ src/actions/projects.js
@@ -1,5 +1,12 @@
 export const loadProjects = () => {
-  return
+  return (dispatch) => {
+    dispatch({ type: 'LOAD_PROJECTS'})
+    fetch('http://localhost:4000/projects')
+      .then(res => res.json())
+      .then(resJSON => {
+        dispatch({ type: 'ADD_PROJECTS', projects: resJSON.data})
+      })
+  }
 }
 
 export const addProject = project => {
