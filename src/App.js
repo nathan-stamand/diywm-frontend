bcb11d85104675002d4d2c8fdd88b2062304f5be
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 11:34:15 -0500

    added import for BrowserRouter, Route, and ProjectPage, edited #handleLoadProjects to accept routerProps as param and pass it to ProjectPage when rendered, Placed a Router in return stmtwith routes for home and projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { loadProjects } from './actions/projects'
-import ProjectList from './containers/projectList'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import ProjectPage from './containers/projectPage'
 import './App.css';
 
 class App extends Component{
@@ -10,21 +14,27 @@ class App extends Component{
     this.props.loadProjects()
   }
 
-  handleLoadProjects = () => {
+  handleLoadProjects = (routerProps) => {
     if (this.props.loading) {
       return <div>Loading...</div>
     }
     else {
-      return <div><ProjectList projects={this.props.projects} /></div>
+      return <ProjectPage {...routerProps} projects={this.props.projects} />
     }
   }
 
   render () {
     return (
-      <div className="App">
-        <h1>Projects</h1>
-        {this.handleLoadProjects()}
-      </div>
+      <Router>
+        <div className="App">
+          {/* <NavBar /> */}
+          <Route exact path="/" render={() => <div>HomePage</div>} />
+          <Route path='/projects' render={routerProps => {
+            return this.handleLoadProjects(routerProps)
+          }}
+          />
+        </div>
+      </Router>
     );
   }
 }
