88f246ad6b4af54da81b487f21ba10875afbbd0c
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 13:48:06 -0500

    spacing refactored in h3 materials section

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -10,9 +10,7 @@ const ProjectShow = projects => {
         <h3>Blog</h3>
           <p>{project.attributes.blog}</p>
         <h3>Materials</h3>
-          <p>
-            {project.attributes.materials}
-          </p>
+          <p>{project.attributes.materials}</p>
         <h3>Time Required</h3>
           <p>{project.attributes.total_time} minute(s)</p>
       </div>
