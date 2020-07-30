14a8631e9335d236d297a71521da561d89b3a0c3
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 13:46:11 -0500

    added Blog, Materials, and Time Required upon rendering project successfully

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -7,6 +7,14 @@ const ProjectShow = projects => {
     return (
       <div id="project-display">
         <h1>{project.attributes.name}</h1>
+        <h3>Blog</h3>
+          <p>{project.attributes.blog}</p>
+        <h3>Materials</h3>
+          <p>
+            {project.attributes.materials}
+          </p>
+        <h3>Time Required</h3>
+          <p>{project.attributes.total_time} minute(s)</p>
       </div>
     )
   }
