5bfdf51ebafcdfe077b16e1dcfeb86bb737849d6
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 11:28:07 -0500

    deleted ProjectList from containers, created ProjectPage to render ProjectList and a Route that will show individual projects

---
+++ src/containers/projectPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import ProjectList from "../components/projects/projectList";
+import ProjectShow from "../components/projects/projectShow";
+
+const ProjectPage = ({ match, projects }) => {
+    return (
+      <div>
+        <ProjectList projects={projects} />
+        <Route path={`${match}/:projectId`} render={routerProps => <ProjectShow {...routerProps} projects={projects} /> }/>
+      </div>
+    )
+}
+
+export default ProjectPage;
