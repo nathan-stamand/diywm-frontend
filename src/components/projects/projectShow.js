53d6cf0c73c3c95aa962c9750a1fe81a8b27d5c0
nathan-stamand <nathansaint810@gmail.com>
2020-07-30 12:08:50 -0500

    added const id & project to find project based on the params in the address bar, and a conditional to return the title of the project if one is found, else it returns No Project Found in a h1

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 const ProjectShow = projects => {
-  return (
-    <div></div>
-  )
+  const id = projects.match.params.projectId
+  const project = projects.projects.find(proj => proj.id === id)
+  if (project) {
+    return (
+      <div id="project-display">
+        <h1>{project.attributes.name}</h1>
+      </div>
+    )
+  }
+  else {
+    return (
+      <h1>No Project Found</h1>
+    )
+  }
 }
 
 export default ProjectShow;
