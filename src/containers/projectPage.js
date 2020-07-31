c941fea4ea105193fa02ab5c71aa228835aff51c
nathan-stamand <nathansaint810@gmail.com>
2020-07-31 16:17:23 -0500

    changed to a class component

--- src/containers/projectPage.js
+++ src/containers/projectPage.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import ProjectList from "../components/projects/projectList";
 import ProjectShow from "../components/projects/projectShow";
 
-const ProjectPage = ({ match, projects }) => {
-  return (
-    <div>
-      <ProjectList projects={projects} />
-      <Route path={`/:projectId`} render={routerProps => {
-        return <ProjectShow {...routerProps} projects={projects} /> 
-      }} />
-    </div>
-  )
+class ProjectPage extends Component {
+  render() {
+    return (
+      <div>
+        <ProjectList projects={this.props.projects} />
+        <Route path={`/:projectId`} render={routerProps => {
+          return <ProjectShow {...routerProps} deleteProject={this.props.deleteProject} projects={this.props.projects} /> 
+        }} />
+      </div>
+    )
+  }
 }
 
 export default ProjectPage;
