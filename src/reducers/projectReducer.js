91d4e9fe71a537f9c5226def2c2e4418cc8d6c83
nathan-stamand <nathansaint810@gmail.com>
2020-07-31 13:35:52 -0500

    created case for 'CREATE_PROJECT'

--- src/reducers/projectReducer.js
+++ src/reducers/projectReducer.js
@@ -2,7 +2,7 @@ import cuid from 'cuid';
 
 const projectReducer = (state = {projects: [], loading: false }, action) => {
   switch (action.type) {
-    case 'LOAD_PROJECTS':  
+    case 'LOAD_PROJECTS':
       return {
         ...state,
         projects: [...state.projects],
@@ -14,12 +14,18 @@ const projectReducer = (state = {projects: [], loading: false }, action) => {
         projects: action.projects,
         loading: false
       }
+    case 'CREATE_PROJECT':
+      return {
+        ...state,
+        projects: [...state.projects],
+        loading: true
+      }
     case 'ADD_PROJECT':
       let project = {
         key: cuid(),
         ...action.project
       }
-      return [...state, project];
+      return [...state.projects, project];
     case 'DELETE_PROJECT':
       return;
     case 'UPDATE_PROJECT':
