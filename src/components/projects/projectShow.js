4e19555435f82456b996b23466a7d3a3ec6313a4
nathan-stamand <nathansaint810@gmail.com>
2020-07-31 15:23:19 -0500

    switched to be a class Component rather than functional, edited const id and project to work again

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ProjectShow = projects => {
-  const id = projects.match.params.projectId
-  const project = projects.projects.find(proj => proj.id === id)
-  if (project) {
-    return (
-      <div id="project-display">
-        <h1>{project.attributes.name}</h1>
-        <h3>Blog</h3>
-          <p>{project.attributes.blog}</p>
-        <h3>Materials</h3>
-          <p>{project.attributes.materials}</p>
-        <h3>Time Required</h3>
-          <p>{project.attributes.total_time} minute(s)</p>
-      </div>
-    )
-  }
-  else if (id === 'new') {
-    return <h1>New Project</h1>
-  }
-  else {
-    return <h1>Project Not Found</h1>
+class ProjectShow extends Component {
+  render () {
+    const id = this.props.match.params.projectId
+    const project = this.props.projects.find(proj => proj.id === id)
+    if (project) {
+      return (
+        <div id="project-display">
+          <h1>{project.attributes.name}</h1>
+          <h3>Blog</h3>
+            <p>{project.attributes.blog}</p>
+          <h3>Materials</h3>
+            <p>{project.attributes.materials}</p>
+          <h3>Time Required</h3>
+            <p>{project.attributes.total_time} minute(s)</p>
+          <button id="delete-btn">DELETE</button>
+        </div>
+      )
+    }
+    else if (id === 'new') {
+      return <h1>New Project</h1>
+    }
+    else {
+      return <h1>Project Not Found</h1>
+    }
   }
 }
 
