36a9a8d97a86db42ed678eb182cd5cc9d8a4ea26
nathan-stamand <nathansaint810@gmail.com>
2020-07-31 21:50:31 -0500

    changed back to a functional component

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,29 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Redirect } from "react-router-dom";
 
-class ProjectShow extends Component {
-  render () {
-    const id = this.props.match.params.projectId
-    const project = this.props.projects.find(proj => proj.id === id)
-    if (project) {
-      return (
-        <div id="project-display">
-          <h1>{project.attributes.name}</h1>
-          <h3>Blog</h3>
-            <p>{project.attributes.blog}</p>
-          <h3>Materials</h3>
-            <p>{project.attributes.materials}</p>
-          <h3>Time Required</h3>
-            <p>{project.attributes.total_time} minute(s)</p>
-        </div>
-      )
-    }
-    else if (id === 'new') {
-      return <h1>New Project</h1>
-    }
-    else {
-      return <Redirect to="/"></Redirect>
-    }
+const ProjectShow = (props) => {
+  const id = props.match.params.projectId
+  const project = props.projects.find(proj => proj.id === id)
+  if (project) {
+    return (
+      <div id="project-display">
+        <h1>{project.attributes.name}</h1>
+        <h3>Blog</h3>
+          <p>{project.attributes.blog}</p>
+        <h3>Materials</h3>
+          <p>{project.attributes.materials}</p>
+        <h3>Time Required</h3>
+          <p>{project.attributes.total_time} minute(s)</p>
+      </div>
+    )
+  }
+  else if (id === 'new') {
+    return <h1>New Project</h1>
+  }
+  else {
+    return <Redirect to="/"></Redirect>
   }
 }
 
