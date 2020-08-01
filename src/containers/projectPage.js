5203c916acbf61c92a4219d528d1203a38c41c67
nathan-stamand <nathansaint810@gmail.com>
2020-08-01 10:49:46 -0500

    defined and called #findRoute to handle returning routes based on the pathname of the page

--- src/containers/projectPage.js
+++ src/containers/projectPage.js
@@ -2,15 +2,33 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import ProjectList from "../components/projects/projectList";
 import ProjectShow from "../components/projects/projectShow";
+import ProjectInput from "../components/projects/projectInput";
 
 class ProjectPage extends Component {
+  findRoute = () => {
+    const id = this.props.location.pathname.split('/').slice(1)
+    const project = this.props.projects.find(proj => proj.id === id[0])
+    if (id[0] === 'new') {
+      return <Route path={'/new'} render={routerProps => {
+        return <ProjectInput {...routerProps} />
+      }} />
+    }
+    else if (project && id[1] === 'edit') {
+      console.log('bruh we out here')
+      return <div></div>
+    }
+    else if (project && !id[1]) {
+      return <Route path={`/:projectId`} render={routerProps => {
+        return <ProjectShow {...routerProps} projects={this.props.projects} /> 
+      }} />
+    }
+  }
+
   render() {
     return (
       <div>
         <ProjectList projects={this.props.projects} />
-        <Route path={`/:projectId`} render={routerProps => {
-          return <ProjectShow {...routerProps} projects={this.props.projects} /> 
-        }} />
+        {this.findRoute()}
       </div>
     )
   }
