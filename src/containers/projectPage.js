8c957799b0b77f9826d482a615c772c8d9b3b4d6
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 11:57:23 -0500

    added Router w/ Switch to pull paths for /new (ProjectInput) and /:projectId (ProjectShow)

--- src/containers/projectPage.js
+++ src/containers/projectPage.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import ProjectList from "../components/projects/projectList";
+import ProjectShow from "../components/projects/projectShow";
+import ProjectInput from "../components/projects/projectInput";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
 class ProjectPage extends Component {
   render() {
     return (
       <div>
-        <ProjectList projects={this.props.projects} />
-        <button id='new-proj-btn' onClick={() => this.redirectNewProject()}>+</button>
+        <Router>
+          <ProjectList projects={this.props.projects} />
+          <button id='new-proj-btn' onClick={() => console.log('new project')}>+</button>
+          <Switch>
+            <Route path={`/new`}>
+              <ProjectInput />
+            </Route>
+            <Route path={`/:projectId`}>
+              <ProjectShow projects={this.props.projects}/>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
