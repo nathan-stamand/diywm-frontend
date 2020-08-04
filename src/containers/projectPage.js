d98d7a3e87158ce0cd1a61cfa53dd32a987ea922
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 12:07:56 -0500

    added route for ProjectEdit

--- src/containers/projectPage.js
+++ src/containers/projectPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ProjectList from "../components/projects/projectList";
 import ProjectShow from "../components/projects/projectShow";
 import ProjectInput from "../components/projects/projectInput";
+import ProjectEdit from "../components/projects/ProjectEdit";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
 class ProjectPage extends Component {
@@ -14,6 +15,9 @@ class ProjectPage extends Component {
           <Switch>
             <Route path={`/new`}>
               <ProjectInput />
+            </Route>
+            <Route path={`/:projectId/edit`}>
+              <ProjectEdit projects={this.props.projects}/>
             </Route>
             <Route path={`/:projectId`}>
               <ProjectShow projects={this.props.projects}/>
