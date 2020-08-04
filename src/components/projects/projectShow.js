d3310353f905a3d63beef43e22b334a7184a2476
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 14:50:42 -0500

    changed to a class component, added #handleEdit to redirect to a project's edit page, added button with onClick to call it

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import { withRouter, Redirect } from "react-router-dom";
 
-const ProjectShow = (props) => {
-  const id = props.match.params.projectId;
-  const project = props.projects.find(proj => proj.id === id)
-  if (project) {
-    return (
-      <div id="project-display">
-        <h1>{project.attributes.name}</h1>
-        <h3>Blog</h3>
-          <p>{project.attributes.blog}</p>
-        <h3>Materials</h3>
-          <p>{project.attributes.materials || 'None'}</p>
-        <h3>Time Required</h3>
-          <p>{project.attributes.total_time || '0'} minute(s)</p>
-      </div>
-    )
+class ProjectShow extends Component {
+  handleEdit = project => {
+    this.props.history.push(`${project.id}/edit`)
   }
-  else {
-    return <Redirect to='/' />
+  render() {
+    const id = this.props.match.params.projectId;
+    const project = this.props.projects.find(proj => proj.id === id)
+    if (project) {
+      return (
+        <div id="project-display">
+          <h1>{project.attributes.name}</h1>
+          <h3>Blog</h3>
+            <p>{project.attributes.blog}</p>
+          <h3>Materials</h3>
+            <p>{project.attributes.materials || 'None'}</p>
+          <h3>Time Required</h3>
+            <p>{project.attributes.total_time || '0'} minute(s)</p>
+          <button id="edit-project-btn" onClick={() => this.handleEdit(project)}>EDIT</button>
+        </div>
+      )
+    }
+    else {
+      return <Redirect to='/' />
+    }
   }
 }
 
