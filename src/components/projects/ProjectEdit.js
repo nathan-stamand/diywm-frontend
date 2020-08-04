1aa72e44a5a1eea7c94f0959d1613b228dacb2fb
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 15:13:13 -0500

    added #componentDidMount to set initisal state or redirect, added labels to inputs plus a submit input and wrapped all in a form, added a form listener (submit) to call handleSubmit, created #handleSubmit to prevent default behavior

--- src/components/projects/ProjectEdit.js
+++ src/components/projects/ProjectEdit.js
@@ -7,16 +7,39 @@ class ProjectEdit extends Component {
     blog: ''
   }
 
+  componentDidMount = () => {
+    const id = this.props.match.params.projectId
+    const project = this.props.projects.find(proj => proj.id === id)
+    if (project) {
+      this.setState({
+        name: project.attributes.name,
+        blog: project.attributes.blog
+      })
+    }
+    else {
+      this.props.history.push('/')
+    }
+  }
+
   handleChange = event => {
     this.setState({
       [event.target.id]: event.target.value
     })
   }
+
+  handleSubmit = event => {
+    event.preventDefault()
+  }
   render () {
     return (
       <div>
-        <input type="text" id="name" value={this.state.name} onChange={this.handleChange}/>
-        <textarea id="blog" value={this.state.blog} onChange={this.handleChange} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Project Name:</label>
+          <input type="text" id="name" value={this.state.name} onChange={this.handleChange}/>
+          <label htmlFor="blog">Blog:</label>
+          <textarea id="blog" value={this.state.blog} onChange={this.handleChange} />
+          <input type="submit" value="Save Changes" />
+        </form>
       </div>
     )
   }
