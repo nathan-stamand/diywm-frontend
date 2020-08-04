495a58749f30a8ed1e4bbe7107befa2b3315b17d
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 11:50:26 -0500

    revamped return to be conditional (for now -- eventually will redirect and uncommented out return values. Added conditionals for values with null

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,20 +1,25 @@
 import React from "react";
-// import { Redirect } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 const ProjectShow = (props) => {
-  const id = props.location.pathname.split('/').slice(1)
-  const project = props.projects.find(proj => proj.id === id[0])
-  return (
-    <div id="project-display">
-      <h1>{project.attributes.name}</h1>
-      <h3>Blog</h3>
-        <p>{project.attributes.blog}</p>
-      <h3>Materials</h3>
-        <p>{project.attributes.materials}</p>
-      <h3>Time Required</h3>
-        <p>{project.attributes.total_time} minute(s)</p>
-    </div>
-  )
+  const id = props.match.params.projectId;
+  const project = props.projects.find(proj => proj.id === id)
+  if (project) {
+    return (
+      <div id="project-display">
+        <h1>{project.attributes.name}</h1>
+        <h3>Blog</h3>
+          <p>{project.attributes.blog}</p>
+        <h3>Materials</h3>
+          <p>{project.attributes.materials || 'None'}</p>
+        <h3>Time Required</h3>
+          <p>{project.attributes.total_time || '0'} minute(s)</p>
+      </div>
+    )
+  }
+  else {
+    return <h3 className='no-project-found'>No Project Found</h3>
+  }
 }
 
-export default ProjectShow;
+export default withRouter(ProjectShow);
