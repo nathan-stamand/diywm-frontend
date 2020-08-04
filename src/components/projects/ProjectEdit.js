5a37cafa8a16965addf8849e1d5c46758945a4b0
nathan-stamand <nathansaint810@gmail.com>
2020-08-04 14:48:05 -0500

    imported withRouter, set up input/textarea for a project's name/blog respectively, each with an onClick listener to call handleChange

--- src/components/projects/ProjectEdit.js
+++ src/components/projects/ProjectEdit.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class ProjectEdit extends Component {
+  state = {
+    name: '',
+    blog: ''
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
+  }
   render () {
     return (
       <div>
-        {console.log(this.props)}
+        <input type="text" id="name" value={this.state.name} onChange={this.handleChange}/>
+        <textarea id="blog" value={this.state.blog} onChange={this.handleChange} />
       </div>
     )
   }
 }
 
-export default ProjectEdit;
+export default withRouter(ProjectEdit);
