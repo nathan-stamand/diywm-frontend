c47943824e7fac2b2f02555e3271275383c5fa3d
nathan-stamand <nathansaint810@gmail.com>
2020-08-05 13:29:23 -0500

    added const mappedSteps to create a list of rendered Steps with keys and steps passed in as props, called mappedSteps in return statement

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -1,13 +1,14 @@
 import React from "react";
 import cuid from 'cuid'
+import Step from "./Step";
 
-const StepList = steps => {
-  const steps = steps.map(step => {
-    step
+const StepList = props => {
+  const mappedSteps = props.steps.map(step => {
+    return <Step key={cuid()} step={step} />
   })
   return (
     <div>
-    
+      {mappedSteps}
     </div>
   )
 }
