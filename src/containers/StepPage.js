75a44942aaee87dbcb62becae69202fe0a48389d
nathan-stamand <nathansaint810@gmail.com>
2020-08-05 14:30:55 -0500

    added Router/Route for rendering StepList

--- src/containers/StepPage.js
+++ src/containers/StepPage.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 import StepList from "../components/steps/StepList";
+import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
 
 class StepPage extends Component {
   render() {
     return (
       <div>
-        <StepList steps={this.props.steps} />
+        <Router>
+          <Route path={`${this.props.match.url}`} render={routerProps => {
+            return <StepList {...routerProps} steps={this.props.steps} />
+          }}>
+          </Route>
+        </Router>
       </div>
     )
   }
 }
 
-export default StepPage
+export default withRouter(StepPage);
