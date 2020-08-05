9fdda195a1311208925231c18ca8fa33b6625a6f
nathan-stamand <nathansaint810@gmail.com>
2020-08-05 16:24:32 -0500

    removed routing, now renders only a StepList

--- src/containers/StepPage.js
+++ src/containers/StepPage.js
@@ -1,17 +1,12 @@
 import React, { Component } from "react";
 import StepList from "../components/steps/StepList";
-import { BrowserRouter as Router, Route, withRouter } from "react-router-dom";
+import { withRouter} from "react-router-dom";
 
 class StepPage extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <Route path={`${this.props.match.url}`} render={routerProps => {
-            return <StepList {...routerProps} steps={this.props.steps} />
-          }}>
-          </Route>
-        </Router>
+        <StepList steps={this.props.steps} />
       </div>
     )
   }
