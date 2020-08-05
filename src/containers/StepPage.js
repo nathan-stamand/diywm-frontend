65d39419fa1256c38da5f45cfccb3e7974a8cdb4
nathan-stamand <nathansaint810@gmail.com>
2020-08-05 08:48:54 -0500

    created class component for StepPage that console.logs its props

---
+++ src/containers/StepPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class StepPage extends Component {
+  render() {
+    return (
+      <div>
+        {console.log(this.props)}
+      </div>
+    )
+  }
+}
+
+export default StepPage
