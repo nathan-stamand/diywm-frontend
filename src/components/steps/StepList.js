d4f730f2c2d143bc82f5750fc0289130a639fe6c
nathan-stamand <nathansaint810@gmail.com>
2020-08-05 09:58:31 -0500

    created StepList functional component

---
+++ src/components/steps/StepList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import cuid from 'cuid'
+
+const StepList = steps => {
+  const steps = steps.map(step => {
+    step
+  })
+  return (
+    <div>
+    
+    </div>
+  )
+}
+
+export default StepList;
