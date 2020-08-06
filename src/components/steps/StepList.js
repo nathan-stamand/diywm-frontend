673cd0997ab8741bbfc2c98062193a1eec14ab39
nathan-stamand <nathansaint810@gmail.com>
2020-08-06 08:57:00 -0500

    converted to class component

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import cuid from 'cuid'
 import Step from "./Step";
 import { withRouter } from "react-router-dom";
 
-const StepList = props => {
-  const mappedSteps = props.steps.map(step => {
-    return <Step key={cuid()} step={step} />
-  })
-  return (
-    <div>
-      {mappedSteps}
-    </div>
-  )
+class StepList extends Component {
+  mappedSteps = () => {
+    return this.props.steps.map(step => {
+      return <Step key={cuid()} step={step} />
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.mappedSteps()}
+      </div>
+    )
+  }
 }
 
 export default withRouter(StepList);
