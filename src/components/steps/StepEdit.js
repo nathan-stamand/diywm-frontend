22a0205b137a627f675b614e4226269c9202972b
nathan-stamand <nathansaint810@gmail.com>
2020-08-06 12:35:47 -0500

    created class component

---
+++ src/components/steps/StepEdit.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class StepEdit extends Component {
+  render() {
+    return (
+      <div>{console.log(this.props)}</div>
+    )
+  }
+}
+
+export default StepEdit;
