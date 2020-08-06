b6ce5118ddeadc68531a58b89217b14349235fc0
nathan-stamand <nathansaint810@gmail.com>
2020-08-06 10:13:40 -0500

    removed BrowserRouter (Router) as it's redundant

--- src/containers/projectPage.js
+++ src/containers/projectPage.js
@@ -3,26 +3,24 @@ import ProjectList from "../components/projects/ProjectList";
 import ProjectShow from "../components/projects/ProjectShow";
 import ProjectInput from "../components/projects/ProjectInput";
 import ProjectEdit from "../components/projects/ProjectEdit";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 class ProjectPage extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <ProjectList projects={this.props.projects} />
-          <Switch>
-            <Route path={`/new`}>
-              <ProjectInput />
-            </Route>
-            <Route path={`/:projectId/edit`}>
-              <ProjectEdit projects={this.props.projects}/>
-            </Route>
-            <Route path={`/:projectId`}>
-              <ProjectShow projects={this.props.projects}/>
-            </Route>
-          </Switch>
-        </Router>
+        <ProjectList projects={this.props.projects} />
+        <Switch>
+          <Route path={`/new`}>
+            <ProjectInput />
+          </Route>
+          <Route path={`/:projectId/edit`}>
+            <ProjectEdit projects={this.props.projects}/>
+          </Route>
+          <Route path={`/:projectId`}>
+            <ProjectShow projects={this.props.projects}/>
+          </Route>          
+        </Switch>
       </div>
     )
   }
