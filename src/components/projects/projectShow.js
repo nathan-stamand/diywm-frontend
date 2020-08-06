e89209df1e3bbbc6e46018cbc45cef5d662cefd3
nathan-stamand <nathansaint810@gmail.com>
2020-08-06 10:00:39 -0500

    added #handleShowSteps , a button to call it when clicked, added a Route to render StepPage with props for the project being shown and its steps

--- src/components/projects/projectShow.js
+++ src/components/projects/projectShow.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
-import { withRouter, Redirect } from "react-router-dom";
+import { withRouter, Redirect, Route } from "react-router-dom";
 import StepPage from "../../containers/StepPage";
 
 class ProjectShow extends Component {
-  handleEdit = project => {
-    this.props.history.push(`${project.id}/edit`)
+  handleEdit = () => {
+    this.props.history.push(`${this.props.match.url}/edit`)
   }
+
+  handleShowSteps = project => {
+    const url = this.props.location.pathname.split('/')
+    if (url.find(el => el === 'steps')) {
+      this.props.history.push(`/${project.id}`)
+    }
+    else {
+      this.props.history.push(`${this.props.match.url}/steps`)
+    }
+  }
+  
   render() {
     const id = this.props.match.params.projectId;
     const project = this.props.projects.find(proj => proj.id === id)
@@ -19,8 +30,11 @@ class ProjectShow extends Component {
             <p>{project.attributes.materials || 'None'}</p>
           <h3>Time Required</h3>
             <p>{project.attributes.total_time || '0'} minute(s)</p>
-          <button id="edit-project-btn" onClick={() => this.handleEdit(project)}>EDIT</button>
-          <StepPage project={project} steps={project.attributes.steps}/>
+          <button id="edit-project-btn" onClick={() => this.handleEdit()}>EDIT</button>
+          <button id="show-hide-steps" onClick={() => this.handleShowSteps(project)}>SHOW/HIDE STEPS</button>
+          <Route path={`${this.props.match.url}/steps`} render={() =>
+            <StepPage project={project} steps={project.attributes.steps} />
+          } />
         </div>
       )
     }
