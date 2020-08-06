e9c60c7a8a3760d605883685da24e7bd2f563fed
nathan-stamand <nathansaint810@gmail.com>
2020-08-06 12:34:41 -0500

    added conditional in #mappedSteps to render StepEdit or Step based on the current location

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import cuid from 'cuid'
 import Step from "./Step";
+import StepEdit from './StepEdit'
 import { withRouter } from "react-router-dom";
 
 class StepList extends Component {
   mappedSteps = () => {
+    const pathArray = this.props.location.pathname.split('/').slice(3)
     return this.props.steps.map(step => {
-      return <Step key={cuid()} step={step} />
+      if (step.id === parseInt(pathArray[0]) && pathArray[1] === 'edit') {
+        return <StepEdit key={cuid()} step={step} />
+      }
+      else {
+        return <Step key={cuid()} step={step} />
+      }
     })
   }
 
