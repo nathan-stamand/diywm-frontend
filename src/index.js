33b3a5fc7427971134a77e85e1481dee5c204bad
nathan-stamand <nathansaint810@gmail.com>
2020-08-07 10:22:16 -0500

    created rootReducer to combine projectReducer and stepReducer (through combineReducer) and set rootReducer as the reducer called in #createStore

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import projectReducer from './reducers/projectReducer.js';
+import stepReducer from "./reducers/stepReducer.js";
 import thunk from "redux-thunk";
 
-const store = createStore(projectReducer, applyMiddleware(thunk))
+const rootReducer = combineReducers({
+  projects: projectReducer,
+  steps: stepReducer
+})
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
