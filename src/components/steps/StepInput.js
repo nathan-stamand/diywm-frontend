1aefe95147ba0ca38029858e720c731331dd0aed
nathan-stamand <nathansaint810@gmail.com>
2020-08-10 14:45:11 -0500

    added #handleChange and #componentDidMount to go with the new state to make input values change with state

--- src/components/steps/StepInput.js
+++ src/components/steps/StepInput.js
@@ -2,18 +2,39 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
 class StepInput extends Component {
+  state = {
+    header: '',
+    materials: '',
+    time: '',
+    directions: '',
+    project_id: ''
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      project_id: this.props.project.id
+    })
+  }
+
   handleSubmit = event => {
     event.preventDefault()
+    console.log(this.state, this.props.project)
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    })
   }
   
   render () {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          Header: <input type="text" id="header" />
-          Materials: <input type="text" id="materials" />
-          Time: <input type="number" id="time" />
-          Directions: <textarea id="directions" />
+          Header: <input type="text" id="header" value={this.state.header} onChange={this.handleChange} />
+          Materials: <input type="text" id="materials" value={this.state.materials} onChange={this.handleChange} />
+          Time: <input type="number" id="time" value={this.state.time} onChange={this.handleChange} />
+          Directions: <textarea id="directions" value={this.state.directions} onChange={this.handleChange} />
           <input type='submit' value='SAVE STEP' />
         </form>
       </div>
