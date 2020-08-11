de3ee936133a4bb49ca3253d02cdd7cf4f17d76a
nathan-stamand <nathansaint810@gmail.com>
2020-08-11 09:16:22 -0500

    created #addStep and corrected #loadSteps' 'ADD_STEPS' dispatch call to call data on resJSON

--- src/actions/steps.js
+++ src/actions/steps.js
@@ -4,7 +4,23 @@ export const loadSteps = () => {
     fetch('http://localhost:4000/steps')
       .then(res => res.json())
       .then(resJSON => {
-        dispatch({type: 'ADD_STEPS', steps: resJSON})
+        dispatch({type: 'ADD_STEPS', steps: resJSON.data})
       })
+  }
+}
+
+export const addStep = step => {
+  const stepObj = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(step)
+  }
+  return (dispatch) => {
+    dispatch({type: 'ADD_STEP', step});
+    fetch(`http://localhost:4000/projects/${step.project_id}/steps`, stepObj)
+      .then(() => dispatch(loadSteps()))
   }
 }
