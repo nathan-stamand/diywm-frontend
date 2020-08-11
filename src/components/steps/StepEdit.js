cd26ebdd5f264508dfda316abb1c5a8ff6477648
nathan-stamand <nathansaint810@gmail.com>
2020-08-11 16:43:23 -0500

    called withRouter in export statement, created #handleSubmit to preventdefault when form is submitted and log props, added a form (which calls handleSubmit when submitted) with inputs for header, materials, time, and directions, added a submit button

--- src/components/steps/StepEdit.js
+++ src/components/steps/StepEdit.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
 class StepEdit extends Component {
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log(this.props)
+  }
+
   render() {
     return (
-      <div>{console.log(this.props)}</div>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          Header: <input type="text" id="header" />
+          Materials: <input type="text" id="materials" />
+          Time: <input type="number" id="time" />
+          Directions: <textarea type="text" id="directions" />
+          <input type="submit" value="Save Changes" />
+        </form>
+      </div>
     )
   }
 }
 
-export default StepEdit;
+export default withRouter(StepEdit);
