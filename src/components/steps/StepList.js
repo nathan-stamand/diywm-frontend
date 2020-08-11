84004a9d2b324dcbfa1c4eb5d03b84a15868511c
nathan-stamand <nathansaint810@gmail.com>
2020-08-11 10:06:48 -0500

    added id to rendered StepEdit and Step

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -9,10 +9,10 @@ class StepList extends Component {
     const pathArray = this.props.location.pathname.split('/').slice(3)
     return this.props.steps.map(step => {
       if (step.id === parseInt(pathArray[0]) && pathArray[1] === 'edit') {
-        return <StepEdit key={cuid()} step={step} />
+        return <StepEdit key={cuid()} id={step.id} step={step.attributes} />
       }
       else {
-        return <Step key={cuid()} step={step} />
+        return <Step key={cuid()} id={step.id} step={step.attributes} />
       }
     })
   }
