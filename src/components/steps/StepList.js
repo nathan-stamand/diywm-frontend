c13f9a9bdad76b867be13ca91896590b5af915f8
nathan-stamand <nathansaint810@gmail.com>
2020-08-11 16:45:33 -0500

    removed parseInt from if statement in #mappedSteps

--- src/components/steps/StepList.js
+++ src/components/steps/StepList.js
@@ -8,7 +8,7 @@ class StepList extends Component {
   mappedSteps = () => {
     const pathArray = this.props.location.pathname.split('/').slice(3)
     return this.props.steps.map(step => {
-      if (step.id === parseInt(pathArray[0]) && pathArray[1] === 'edit') {
+      if (step.id === pathArray[0] && pathArray[1] === 'edit') {
         return <StepEdit key={cuid()} id={step.id} step={step.attributes} />
       }
       else {
