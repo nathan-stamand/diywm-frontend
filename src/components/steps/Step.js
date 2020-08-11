b9f2538828f03cc36e433c8115029f3cd976ff46
nathan-stamand <nathansaint810@gmail.com>
2020-08-11 15:32:12 -0500

    changed const id in #handleEdit/#handleDelete to call on props.id, created const projectId from the url in #handleDelete, and created const ids to be an object with both id's to use as a param in deleteStep

--- src/components/steps/Step.js
+++ src/components/steps/Step.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import { deleteStep } from "../../actions/steps";
+import { compose } from "redux";
+import { connect } from "react-redux";
 
 class Step extends Component {
   handleEdit = () => {
-    const id = this.props.step.id
+    const id = this.props.id
     this.props.history.push(`${this.props.match.url}/${id}/edit`)
+  }
+
+  handleDelete = () => {
+    const id = this.props.id
+    const projectId = this.props.match.url.split('/')[1]
+    const ids = {project: projectId, step: id}
+    this.props.deleteStep(ids)
   }
 
   render() {
@@ -19,10 +29,16 @@ class Step extends Component {
         <p>Time: {time}</p>
         <p>Directions: {directions}</p>
         <button id="edit-step" onClick={() => this.handleEdit()}>EDIT STEP</button>
-        <button id="delete-step">DELETE STEP</button>
+        <button id="delete-step" onClick={() => this.handleDelete()}>DELETE STEP</button>
       </div>
     )
   }
 }
 
-export default withRouter(Step);
+const mapDispatchToProps = dispatch => ({
+  deleteStep: payload => dispatch(deleteStep(payload))
+})
+
+export default compose(
+  withRouter,
+  connect(null, mapDispatchToProps))(Step);
