3a6ce962c2b7e47ea275c04e2a406b4dbd7ead44
nathan-stamand <nathansaint810@gmail.com>
2020-08-17 10:45:41 -0500

    converted to stateless component

--- src/components/projects/projectList.js
+++ src/components/projects/projectList.js
@@ -1,47 +1,31 @@
-import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
-import { deleteProject } from "../../actions/projects";
-import { connect } from 'react-redux';
-import { compose } from "redux";
+import React from "react";
+import { Link } from "react-router-dom";
+import NewProject from "../projects/buttons/NewProject";
+import DeleteProject from "../projects/buttons/DeleteProject";
 import cuid from "cuid";
 
-class ProjectList extends Component {
-  renderProjects = () => this.props.projects.map((project, index) => {
+const ProjectList = props => {
+  const renderProjects = () => props.projects.map((project, index) => {
     return (<li key={cuid()} className="project-link" >
-      <button className="delete-btn" path="/" onClick={() => this.handleDelete(project)}>DELETE</button>
+      <DeleteProject handleDelete={() => props.handleDelete(project)}/>
       <Link to={`/${project.id}`}>
-        {this.props.projects[index].attributes.name}
+        {props.projects[index].attributes.name}
       </Link>
     </li>)})
 
-  handleDelete = project => {
-    this.props.deleteProject(project.id)
-  }
-
-  handleNew = () => {
-    this.props.history.push('/new')
-  }
-
-  render() {
-    return (
-      <div id="project container">
-        <nav id="sidebar">
-          <div id="sidebar-header">
-            <h3>My Projects</h3>
-          </div>
-          <ul className="my-projects-list">
-            {this.renderProjects()}
-          </ul>
-          <button id='new-project-btn' onClick={this.handleNew}>+</button>
-        </nav>
-      </div>
-    )
-  }
+  return (
+    <div id="project container">
+      <nav id="sidebar">
+        <div id="sidebar-header">
+          <h3>My Projects</h3>
+        </div>
+        <ul className="my-projects-list">
+          {renderProjects()}
+        </ul>
+        <NewProject handleNew={props.handleNew} />
+      </nav>
+    </div>
+  )
 }
 
-const mapDispatchToProps = dispatch => ({
-  deleteProject: payload => dispatch(deleteProject(payload))
-})
-
-
-export default compose(withRouter, connect(null, mapDispatchToProps))(ProjectList);
+export default ProjectList;
